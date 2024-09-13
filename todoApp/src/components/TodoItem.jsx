06dace7a0d12d3da02559030271941352cd4f680
Shadkoech <shadrackipruto@gmail.com>
2024-09-13 09:09:36 +0300

    Deleting an item from list

--- todoApp/src/components/TodoItem.jsx
+++ todoApp/src/components/TodoItem.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./todoitem.module.css";
 
-const TodoItem = ({ item }) => {
+const TodoItem = ({ item, todos, setTodos }) => {
+
+  function handleDelete(item) {
+    console.log("Delete button clicked for item", item)
+   setTodos(todos.filter((todo) => todo !== item)); 
+  }
+
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>x</button>
+          <button onClick={() => handleDelete(item)} className={styles.deleteButton}>x</button>
         </span>
       </div>
 
