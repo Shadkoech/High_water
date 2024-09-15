0c628b6d34b542a5a41ace5af25237f661daf059
Shadkoech <shadrackipruto@gmail.com>
2024-09-15 13:13:14 +0300

    Creating search and nav components on the foodApp

---
+++ FoodApp/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./nav.module.css"
+
+const Nav = () => {
+  return (
+    <div className={styles.nav}>
+    🍽️ FoodApp
+    </div>
+  )
+}
+
+export default Nav
--- FoodApp/src/components/Search.jsx
+++ FoodApp/src/components/Search.jsx
@@ -1,9 +1,10 @@
 import { useState, React, useEffect } from "react";
+import styles from "./search.module.css"
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "46ab417f24854a3285bb07e264933c4e";
 
-const Search = () => {
+const Search = ({foodData, setFoodData}) => {
   const [query, setQuery] = useState("pizza");
 
   // syntax of the useEffect hook
@@ -14,12 +15,14 @@ const Search = () => {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data.results);
+      setFoodData(data.results)
     }
     fetchFood();
   }, [query]);
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
+      className={styles.input}
         value={query}
         type="text"
         onChange={(e) => setQuery(e.target.value)}
