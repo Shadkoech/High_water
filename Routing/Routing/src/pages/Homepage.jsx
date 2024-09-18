ed69cd2b9fae2cb4b896a2b3e488b2ef3589e3b3
Shadkoech <shadrackipruto@gmail.com>
2024-09-18 12:26:07 +0300

    The useNavigate, useLocation hooks

--- Routing/Routing/src/pages/Homepage.jsx
+++ Routing/Routing/src/pages/Homepage.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Homepage = () => {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/dashboard/profile", {state: {username}});
+  }
   return (
     <div>
       This is the homepage
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <button onClick={handleClick}>To profile page</button>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
